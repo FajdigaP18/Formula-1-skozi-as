2da0eb611f2dbf0a7e7f94450fefe6a0523bc064
Polona <polona.fajdiga@student.fmf.uni-lj.si>
2022-12-10 23:37:34 +0100

    koda za dodajanje elementov iz csv v bazo.

--- data_import.py
+++ data_import.py
@@ -1,4 +1,67 @@
 import sqlite3
+import csv
+
+def napolni_dirkaci():
+    with open("archive/drivers.csv", "r") as dirkaci:
+        csv_reader = csv.reader(dirkaci, delimiter = ',')
+        naslovi_st = next(csv_reader)
+        seznam = []
+        caunt = 0
+        for vrstica in csv_reader:
+            if caunt == 5: break
+            caunt += 1
+            tupl = tuple([vrstica[0]] + vrstica[4:-1])
+            seznam.append(tupl)
+        sql = "INSERT INTO dirkaci (did, priimek, short_name, dateofbrith, nationality) VALUES (?, ?, ?, ?, ?)"
+        cursor.executemany(sql, seznam)
+        db.commit()
+
+
+def napolni_dirkalisca():
+    with open("archive/circuits.csv", 'r') as dirkalisca:
+        csv_reader = csv.reader(dirkalisca, delimiter=',')
+        naslovi_st = next(csv_reader)
+        seznam = []
+        caunt = 0
+        for vrstica in csv_reader:
+            if caunt == 3:
+                break
+            caunt+=1
+            tupl = tuple([vrstica[0]] + vrstica[2:5])
+            seznam.append(tupl)
+        sql = "INSERT INTO dirkalisca (cid, name, location, country) VALUES (?, ?, ?, ?)"
+        cursor.executemany(sql, seznam)
+        db.commit()
+        
+def napolni_dirka():
+    with open("archive/races.csv", "r") as dirka:
+        csv_reader = csv.reader(dirka, delimiter = ',')
+        naslovi_st = next(csv_reader)
+        seznam = []
+        caunt = 0
+        for vrstica in csv_reader:
+            if caunt == 5: break
+            caunt += 1
+            tupl = tuple([vrstica[0], vrstica[3] ,vrstica[5]])
+            seznam.append(tupl)
+        sql = "INSERT INTO dirka (raceid, dirkalisce, date) VALUES (?, ?, ?)"
+        cursor.executemany(sql, seznam)
+        db.commit()
+
+def napolni_ekipa():
+    with open("archive/constructors.csv", "r") as ekipa:
+        csv_reader = csv.reader(ekipa, delimiter = ',')
+        naslovi_st = next(csv_reader)
+        seznam = []
+        caunt = 0
+        for vrstica in csv_reader:
+            if caunt == 5: break
+            caunt += 1
+            tupl = tuple([vrstica[0], vrstica[2], vrstica[3]])
+            seznam.append(tupl)
+        sql = "INSERT INTO ekipa (eid, team_name, nationality) VALUES (?, ?, ?)"
+        cursor.executemany(sql, seznam)
+        db.commit()
 
 dirkaci = "CREATE TABLE IF NOT EXISTS dirkaci (did INTEGER PRIMARY KEY, priimek TEXT, short_name TEXT, dateofbrith DATE, nationality TEXT)"
 dirkalisca = "CREATE TABLE IF NOT EXISTS dirkalisca (cid INTEGER PRIMARY KEY, name TEXT, location TEXT, country TEXT)"
@@ -14,11 +77,15 @@ with db as cursor:
     cursor.execute(dirkalisca)
     cursor.execute(dirka)
     cursor.execute(ekipa)
+    napolni_dirkaci()
+    napolni_dirkalisca()
+    napolni_dirka()
+    napolni_ekipa()
 
-def napolni_dirkaci():
-    with open("archive/drivers.csv", "r") as dirkaci:
-        for vrstica in list(dirkaci)[1:]:
-            id, *_, short_name, ime, priimek, date, country = vrstica.strip().split(",")
-            print(id, short_name, ime, priimek, date, country)
-
-napolni_dirkaci()
+# def napolni_dirkaci():
+#     with open("archive/drivers.csv", "r") as dirkaci:
+#         for vrstica in list(dirkaci)[1:]:
+#             id, *_, short_name, ime, priimek, date, country = vrstica.strip().split(",")
+#             print(id, short_name, ime, priimek, date, country)
+# 
+# napolni_dirkaci()
